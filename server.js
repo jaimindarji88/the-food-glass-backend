2ef6128609ceda229fec9629a24c7f91f45dc1c2
Tyler Posadovsky <Tyler.Posadovsky@kandy.io>
2018-02-03 18:13:34 -0500

    added node server and socket

---
+++ server.js
@@ -0,0 +1,28 @@
+const express   = require('express');
+const app       = express();
+const http      = require('http');
+const server    = require('http').createServer(app);  
+const io        = require('socket.io')(server);
+
+const LISTEN_PORT   = 8080;
+
+server.listen(LISTEN_PORT);
+app.use(express.static(__dirname + '/public')); //set root path of server ...
+
+console.log("Listening on port: " + LISTEN_PORT );
+
+//io events
+io.on('connection', (socket) => {
+
+    console.log(socket.id + " connected");
+
+    socket.on('disconnect', () => {
+        console.log(socket.id + " disconnected");
+    });
+
+    //custom events
+    socket.on('getPic', (data) => {
+        console.log("pic received");
+        io.sockets.emit('pic', {});
+    });
+});
