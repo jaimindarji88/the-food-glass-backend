b2d3c629d1598205c0d433c0149624be4cdb97cb
Tyler Gawalewicz <tygawa@rocketmail.com>
2018-02-03 22:01:49 -0500

    Worked on Watson API & added test photos for Watson

--- api/watson.js
+++ api/watson.js
@@ -4,27 +4,41 @@ APIKEY for food db: LUITmEEltJMaq6s75hXa1SGJIiWAhrn1MVKIikZH
 https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=LUITmEEltJMaq6s75hXa1SGJIiWAhrn1MVKIikZH&location=Denver+CO
 */
 
-var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
+const WatsonAPI = function(picture){
+  var watson = require('watson-developer-cloud');
+  var fs = require('fs');
 
-const WatsonAPI = function(imgFile){
+  var visual_recognition = watson.visual_recognition({
+    api_key: 'de73bb0daa7a4ec91ad49355fac9c0d893a252a3',
+    version: 'v3',
+    version_date: '2016-05-20'
+  });
 
-    var visual_recognition = new VisualRecognitionV3({
-        api_key: '<LUITmEEltJMaq6s75hXa1SGJIiWAhrn1MVKIikZH>',
-        version_date: VisualRecognitionV3.VERSION_DATE_2016_05_20
-      });
-       
-      var params = {
-        images_file: imgFile
-      };
-       
-      visual_recognition.classify(params, function(err, res) {
-        if (err) {
-          console.log(err);
-        } else {
-          return JSON.stringify(res, null, 2);
-        }
+  let parameters = {
+    //classifier_ids: ["fruits_1462128776","SatelliteModel_6242312846"],
+    classifier_ids: ["food"],
+    threshold: 0.6
+  };
+  
+  var params = {
+    //images_file: fs.createReadStream('./fruit2.jpg'),
+    //images_file: fs.createReadStream('./fruit.png'),
+    images_file: fs.createReadStream(picture),
+    parameters: parameters
+  };
+  
+  visual_recognition.classify(params, function(err, response) {
+    if (err)
+      return console.log(err);
+    //console.log(JSON.stringify(response, null, 2))
+    var args = response.images[0].classifiers[0].classes;
+    var newList = []
+    args.forEach(function(element) {
+      newList.push({name: element.class, probability: element.score});
     });
-    
+    return console.log(newList)
+  });
 }
- 
-export default WatsonAPI;
+
+WatsonAPI('./fruit.png')
+//export default WatsonAPI; 
